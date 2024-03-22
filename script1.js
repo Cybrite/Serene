fc37ed09ee33a7950656128d7a26f5fc31e69826
Harsh <140698710+Cybrite@users.noreply.github.com>
2024-03-22 23:19:46 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+alert("THIS IS  A PROTOTYPE!!");
+let a1= document.querySelector("#a1");
+let a2= document.querySelector("#a2");
+let a3= document.querySelector("#a3");
+let a4= document.querySelector("#a4");
+let a5= document.querySelector("#a5");
+let a6= document.querySelector("#a6");
+
+
